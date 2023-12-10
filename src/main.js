10dcba5028026921026e095338e7191a1a61ba57
sxrxhmxsxrxs <sarahmisaras@icloud.com>
2023-12-10 14:15:23 +0100

    feat/vue-refactor: - bootstrap-vue gone

--- src/main.js
+++ src/main.js
@@ -5,9 +5,7 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 const app = createApp(App)
 const pinia = createPinia()
