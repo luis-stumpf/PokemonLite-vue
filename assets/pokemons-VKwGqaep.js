4ca925222a86e8f6ffa3c1396536485e5543c3d9
Luis Stumpf <luis.stumpf@medflex.de>
2023-12-14 22:04:48 +0100

    Updates

---
+++ assets/pokemons-VKwGqaep.js
@@ -0,0 +1,1 @@
+import{G as n,r as t,C as e,g as r}from"./index-UpClggMC.js";const p=n("pokemons",()=>{const o=t(e.pokemonSamples);async function s(){const a=await r.get(`${e.serverUrl}/api/allPokemonsJson`);o.value=a.data}return{pokemons:o,getPokemonsData:s}});export{p as u};
