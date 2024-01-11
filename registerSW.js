6a7e1b65f403e17a80049ecefb7b63219b24dc2e
Luis Stumpf <luis.stumpf@medflex.de>
2024-01-11 14:42:12 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/PokemoLite-vuesw.js', { scope: '/PokemoLite-vue' })})}
